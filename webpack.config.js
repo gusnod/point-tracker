9bce23ffff9029e31952526e96c4e694224bd3ef
haakonaareskjold <aareskjold.hhaakon@gmail.com>
2021-02-14 23:04:16 +0100

    added import for css to index.tsx, added resource dir for scss, added style/css loader for webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,11 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: '/node-modules/'
-            }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+              },
         ],
     },
     output: {
