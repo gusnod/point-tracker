93141b393697528e09d954a9c93daf2716d71e0c
Gustav Nødland <gustav.nodland@gmail.com>
2021-02-18 19:22:56 +0100

    Add automatic focus on input in doRound

--- src/Components/Round.tsx
+++ src/Components/Round.tsx
@@ -11,26 +11,32 @@ const Round = ({ players, submitScore }: IProps) => {
     const [scores, setScores] = useState<IScore[]>([]);
     const [idx, setIdx] = useState<number>(0);
     const [currentScore, setCurrentStore] = useState<string>("");
+    let focus: HTMLInputElement | null = null
+    useEffect(() => {
+        if (focus) focus.focus();
+    }, [])
+    return <form onSubmit={e => {
+        e.preventDefault();
+        const sscore = parseInt(currentScore);
+        const currentPlayer = players[idx]
 
-    return <div>
+        const newScores = [...scores, { player: currentPlayer, score: sscore }];
+        setCurrentStore("");
+        if (idx + 2 > players.length) {
+            submitScore({ scores: newScores })
+        }
+
+        setScores(newScores);
+        setIdx(idx + 1);
+
+    }}>
         <input type="number" value={currentScore} placeholder={players[idx].name} onChange={(e) => {
             setCurrentStore(e.target.value);
-        }} />
-        <button type="button" onClick={() => {
-            const sscore = parseInt(currentScore);
-            const currentPlayer = players[idx]
+        }}
+            ref={(ref) => focus = ref} />
+        <button type="submit" >Submit</button>
+    </form>
 
-            const newScores = [...scores, { player: currentPlayer, score: sscore }];
-            setCurrentStore("");
-            if (idx + 2 > players.length) {
-                submitScore({ scores: newScores })
-            }
-
-            setScores(newScores);
-            setIdx(idx + 1);
-
-        }}>Submit</button>
-    </div>
 }
 
 
